b64245358956359e0ed7ccf4d0f262b6ce27dd14
Ben Willenbring <ben-willenbring@t-online.de>
2021-04-18 05:31:32 +0200

    refactor: improve logging when setting seeds

--- src/adapter/serverProperties.ts
+++ src/adapter/serverProperties.ts
@@ -5,22 +5,46 @@ import { promises } from "fs"
 const FILE_NAME = "server.properties"
 
 export async function initSeed(seeds: string[], configuration: Configuration) {
+	if (seeds?.length === 0 && (await seedExists())) {
+		await resetSeed()
+		return
+	}
+
 	if (seeds?.length > 0) {
 		await setSeed(seeds[0])
 		await removeSeed(configuration)
-	} else {
-		await setSeed("")
 	}
 }
 
 async function setSeed(seed) {
 	console.log(`Setting seed ${seed} ...`)
 	const serverProperties = await promises.readFile(FILE_NAME, "utf-8")
+	const updated = getUpdatedServerProperties(serverProperties, seed)
+
+	await promises.writeFile(FILE_NAME, updated, "utf-8")
+}
+
+async function resetSeed() {
+	console.log("Resetting seed ...")
+	const serverProperties = await promises.readFile(FILE_NAME, "utf-8")
+	const updated = getUpdatedServerProperties(serverProperties)
+
+	await promises.writeFile(FILE_NAME, updated, "utf-8")
+}
+
+async function seedExists() {
+	const serverProperties = await promises.readFile(FILE_NAME, "utf-8")
 	const key = "level-seed="
 	const start = serverProperties.indexOf(key) + key.length
 	const end = serverProperties.indexOf("\n", start)
 
-	const updated = serverProperties.substring(0, start) + seed + serverProperties.substring(end)
+	return end - start > 1
+}
 
-	await promises.writeFile(FILE_NAME, updated, "utf-8")
+function getUpdatedServerProperties(serverProperties: string, seed: string = "") {
+	const key = "level-seed="
+	const start = serverProperties.indexOf(key) + key.length
+	const end = serverProperties.indexOf("\n", start)
+
+	return serverProperties.substring(0, start) + seed + serverProperties.substring(end)
 }
